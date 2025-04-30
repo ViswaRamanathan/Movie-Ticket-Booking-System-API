b1556d424cf99bac05f17640572f1139f2fd2951
ViswaRamanthan <rviswa0047@gmail.com>
2025-04-30 16:19:13 +0530

    Movie entity is created and values are inserted

---
+++ src/main/java/com/example/mtbs/enums/Genre.java
@@ -0,0 +1,18 @@
+package com.example.mtbs.enums;
+
+public enum Genre {
+    ACTION,
+    COMEDY,
+    HORROR,
+    DRAMA,
+    ROMANCE,
+    ADVENTURE,
+    SCI_FI,
+    SPORT,
+    THRILLER,
+    FANTASY,
+    MYSTERY,
+    MUSICALS,
+    WESTERN,
+    ANIMATION
+}
